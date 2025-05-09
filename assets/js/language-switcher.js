419f7b5dd536c02e6aa11734272f8d6ef15c7b64
Aiden <56921042+1036314134@users.noreply.github.com>
2025-05-09 16:32:51 +0800

    修改英文变量命名

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -11,14 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelectorAll(`.masthead__menu-item a .lang-${lang}`).forEach(el => {
         el.style.display = 'inline';
       });
-
-      // // 切换作者栏文本
-      // document.querySelectorAll('.author__desktop a span').forEach(el => {
-      //   el.style.display = 'none';
-      // });
-      // document.querySelectorAll(`.author__desktop a .lang-${lang}`).forEach(el => {
-      //   el.style.display = 'inline';
-      // });
   
       // 切换 Markdown 内容显示
       document.querySelectorAll('.lang-en, .lang-zh').forEach(el => {
